d537745d6a6000c2731effa6d255f78091fffa58
gsuyemoto <gsuyemoto@gmail.com>
2023-01-19 15:27:17 -0800

    Ordering works with all hand vals passed to enum types. Need to implement gathering of winning hands.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ const MAX_NUM_CARDS: usize = 5;
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 enum PokerHands {
-    HighCard([u8; MAX_NUM_CARDS]),
-    Pair([u8; MAX_NUM_CARDS]),
-    TwoPairs([u8; MAX_NUM_CARDS]),
-    ThreeKind([u8; MAX_NUM_CARDS]),
-    Flush([u8; MAX_NUM_CARDS]),
-    Straight([u8; MAX_NUM_CARDS]),
-    FullHouse([u8; MAX_NUM_CARDS]),
-    FourKind([u8; MAX_NUM_CARDS]),
-    StraightFlush([u8; MAX_NUM_CARDS]),
+    HighCard(Vec<u8>),
+    Pair(Vec<u8>),
+    TwoPairs(Vec<u8>),
+    ThreeKind(Vec<u8>),
+    Flush(Vec<u8>),
+    Straight(Vec<u8>),
+    FullHouse(Vec<u8>),
+    FourKind(Vec<u8>),
+    StraightFlush(Vec<u8>),
     RoyalFlush,
 }
 
@@ -22,10 +22,10 @@ fn main() {
     let mut hands = [
             "4D 5S 6S 8D 3C",
             "2S 4C 7S 9H 10H",
-            "3S 4S 5D 6H JH",
+            "3S 4S 5D 7H JH",
             "3H 4H 5C 6C JD",
         ];
-    let mut hands = ["4S 5H 6C 8D KH", "2S 4H 6S 4D JH"];
+    // let mut hands = ["4S 5H 6C 8D KH", "2S 4H 6S 4D JH"];
     winning_hand(&hands);
 }
 
@@ -45,8 +45,10 @@ pub fn winning_hand<'a>(hands: &[&'a str]) {
     let mut answer = HashMap::new();
     answer.insert(0, &all_hand_types[0]);
     
-    all_hand_types.iter()
-        .fold(answer, )
+    // all_hand_types.iter()
+    //     .fold(answer, )
+    
+    println!("{:?}", all_hand_types.iter().max());
 }
 
 const SUITS: [char; 4] = ['H', 'D', 'S', 'C'];
@@ -78,13 +80,13 @@ fn get_card_vals(hand: &str) -> Vec<u8> {
     let cards = cards.replace("K", "=");
     let cards = cards.replace("A", ">");
     let cards = cards.split(' ').collect::<Vec<&str>>();
-    println!("{:?}", cards);
+    // println!("{:?}", cards);
 
     let mut cards = cards.iter()
         .map(|n| n.chars().next().unwrap() as u8)
         .collect::<Vec<u8>>();
     
-    println!("{:?}", cards);
+    // println!("{:?}", cards);
     cards.sort();
     cards
 }
@@ -107,33 +109,20 @@ fn get_hand_type(is_flush: bool, hand_vals: &mut Vec<u8>) -> PokerHands {
         .map(|(_,v)| *v)
         .collect::<Vec<u8>>();
     
-    // get highest val card in the hand for breaking ties and when
-    // hand is nothing (aka high card)
-    let max = match hand_vals.iter().max() {
-        Some(max_in_hand) => *max_in_hand,
-        None              => panic!("No max found!!"),
-    };
-
-    // get highest pair for breaking ties
-    let max_pair = match get_pairs.iter().max() {
-        Some(max)     => *max,
-        None          => 0u8,
-    };
-    
     let mut hand_type = match (is_straight, is_flush) {
-        (true, true)     => if hand_vals[0] == 9 {PokerHands::RoyalFlush} else {PokerHands::StraightFlush(max)},
-        (true, false)    => PokerHands::Straight(max),
-        (false, true)    => PokerHands::Flush(max),
-        (false, false)   => PokerHands::HighCard(max),     
+        (true, true)     => if hand_vals[0] == 9 {PokerHands::RoyalFlush} else {PokerHands::StraightFlush(hand_vals.to_vec())},
+        (true, false)    => PokerHands::Straight(hand_vals.to_vec()),
+        (false, true)    => PokerHands::Flush(hand_vals.to_vec()),
+        (false, false)   => PokerHands::HighCard(hand_vals.to_vec()),     
     };
     
     hand_type = match get_pairs.len() {
-        3 if get_pairs[0] == get_pairs[1] && get_pairs[1] == get_pairs[2] => PokerHands::FourKind(max_pair),
-        3 if get_pairs[0] == get_pairs[1] => PokerHands::FullHouse(max_pair),
-        3 if get_pairs[1] == get_pairs[2] => PokerHands::FullHouse(max_pair),
-        2 if get_pairs[0] == get_pairs[1] => PokerHands::ThreeKind(max_pair),
-        2 => PokerHands::TwoPairs(max_pair),
-        1 => PokerHands::Pair(max_pair),
+        3 if get_pairs[0] == get_pairs[1] && get_pairs[1] == get_pairs[2] => PokerHands::FourKind(hand_vals.to_vec()),
+        3 if get_pairs[0] == get_pairs[1] => PokerHands::FullHouse(hand_vals.to_vec()),
+        3 if get_pairs[1] == get_pairs[2] => PokerHands::FullHouse(hand_vals.to_vec()),
+        2 if get_pairs[0] == get_pairs[1] => PokerHands::ThreeKind(hand_vals.to_vec()),
+        2 => PokerHands::TwoPairs(hand_vals.to_vec()),
+        1 => PokerHands::Pair(hand_vals.to_vec()),
         _ => hand_type,
     };
     
